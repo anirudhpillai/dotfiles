931fd879792eaec164b54c1d10e6f1d176727e1c
Anirudh <anirudhx5@gmail.com>
2016-09-10 18:53:46 +0100

    using fish shell now!

---
+++ fish/config.fish
@@ -0,0 +1,59 @@
+# Path to Oh My Fish install.
+set -q XDG_DATA_HOME
+  and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
+  or set -gx OMF_PATH "$HOME/.local/share/omf"
+
+
+# Load Oh My Fish configuration.
+source $OMF_PATH/init.fish
+
+# Don't need a greeting
+function fish_greeting -d "what's up, fish?"
+end
+
+# Don't want it to show timestamps
+function fish_right_prompt -d "Write out the right prompt"
+    # date '+%H:%M'
+end
+
+
+# Navigation
+function ..    ; cd .. ; end
+function ...   ; cd ../.. ; end
+function ....  ; cd ../../.. ; end
+
+
+# Aliases
+alias d="cd ~/Desktop"
+alias dw="cd ~/Downloads"
+
+alias c="clear"
+
+# Readline colors (Thanks to @paulirish)
+set -g fish_color_autosuggestion 555 yellow
+set -g fish_color_command 5f87d7
+set -g fish_color_comment 808080
+set -g fish_color_cwd 87af5f
+set -g fish_color_cwd_root 5f0000
+set -g fish_color_error 870000 --bold
+set -g fish_color_escape af5f5f
+set -g fish_color_history_current 87afd7
+set -g fish_color_host 5f87af
+set -g fish_color_match d7d7d7 --background=303030
+set -g fish_color_normal normal
+set -g fish_color_operator d7d7d7
+set -g fish_color_param 5f87af
+set -g fish_color_quote d7af5f
+set -g fish_color_redirection normal
+set -g fish_color_search_match --background=purple
+set -g fish_color_status 5f0000
+set -g fish_color_user 5f875f
+set -g fish_color_valid_path --underline
+
+set -g fish_color_dimmed 555
+set -g fish_color_separator 999
+
+set -g fish_pager_color_completion normal
+set -g fish_pager_color_description 555 yellow
+set -g fish_pager_color_prefix cyan
+set -g fish_pager_color_progress cyan
---
+++ fish/functions/fish_prompt.fish
@@ -0,0 +1,1 @@
+/home/anirudh/.local/share/omf/themes/bobthefish/fish_prompt.fish
