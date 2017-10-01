2c1700f738e819c645c61183caf30e7f3489d7ab
Anirudh <anirudhx5@gmail.com>
2017-10-01 20:52:56 +0100

    Updated config to use new brightness scripts

--- polybar/battery.py
+++ polybar/battery.py
@@ -7,8 +7,8 @@ acpi = subprocess.check_output('acpi').decode().split()
 status = acpi[2][:-1]
 level = int(acpi[3].rstrip("%,"))
 
-if status != "Charging" and level < 35:
-    subprocess.run(["xbacklight", "-set", "25"])
-    if level < 25:
+if status != "Charging" and level < 25:
+    subprocess.run(["sudo", "/usr/local/bin/set_brightness", "0.05"])
+    if level < 20:
         subprocess.call(["pkill", "notify-osd"])
         subprocess.call(["notify-send", "Battery Low", "Plug it in!"])
