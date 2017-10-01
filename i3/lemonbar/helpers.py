84ba83c35cd907cad3ad2dceed6c555df9ba25d4
Anirudh <anirudhx5@gmail.com>
2017-10-01 20:52:09 +0100

    Polybar rocks

--- i3/lemonbar/helpers.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-
-def cleanup(app):
-    """Kill all running instances of app"""
-    processes = subprocess.check_output('ps -e', shell=True)
-    if app in processes.decode():
-        pids = subprocess.check_output('pidof ' + app, shell=True)
-        subprocess.run(['kill'] + pids.decode().split())
